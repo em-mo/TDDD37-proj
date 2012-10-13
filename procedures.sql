fabdea4f054840a1aec4ec85043923a98c0c21a4
Emil <karlemil.lindberg@gmail.com>
2012-10-13 13:43:20 +0200

    Probs nothing to keep

--- procedures.sql
+++ procedures.sql
@@ -2,17 +2,17 @@ delimiter //
 
 drop procedure if exists insert_weekly_flight;
 create procedure insert_weekly_flight (in weekday varchar(10), in dep_time time, in arr_time time, in route int, in y year)
-	begin
+begin
 	declare selected_weekday_id int;
 	select w.id into selected_weekday_id from ba_weekday w where weekday = w.name;
 	insert into ba_weekly_flight (weekday_id, departure_time, arrival_time, route_id, flight_year)
 		values (selected_weekday_id, dep_time, arr_time, route, y);
-	end;//
+end;//
 
 ## Work in progress
 drop procedure if exists fill_year_flights//
 create procedure fill_year_flights(in first_day varchar(10), in fill_year year)
-	begin
+begin
 	declare counter int default 1;
 	declare date_counter date;
 	declare leap int;
@@ -22,20 +22,21 @@ create procedure fill_year_flights(in first_day varchar(10), in fill_year year)
 
 	repeat
 
-	set date_counter = date_counter + 1;
-	set counter = counter + 1;
+		set date_counter = date_counter + 1;
+		set counter = counter + 1;
 	until counter <= leap
-	end;//
+	end reapeat;
+end;//
 
 drop procedure if exists leap_year;
 create procedure leap_year(in test_year year, out days int)
-	begin
+begin
 	if((test_year % 4 = 0 AND test_year % 100 != 0) OR test_year % 400 = 0) then
 		set days = 366;
 	else 
 		set days = 365;
 	end if;
-	end;//
+end;//
 
 ##work in progress
 drop procedure if exists reserve;
