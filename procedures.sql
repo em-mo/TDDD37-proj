a10f0c6e31cd578be4d8e54c7d7014c573d53484
Emil <karlemil.lindberg@gmail.com>
2012-10-13 16:34:02 +0200

    Kom igen

--- procedures.sql
+++ procedures.sql
@@ -44,60 +44,19 @@ end;//
 
 ##work in progress
 drop procedure if exists reserve//
-create procedure reserve(in flight_id int, in no_passenger int, out booking_number int)
+create procedure reserve(in flight_id int, in booking_amount int, out booking_number int)
 begin
-
 	declare ticket_amount int;
-	declare booking_amount int;
-	declare booking_id int;
-	declare contact int;
-	declare id1, age1 int default 0;
-	declare first_name1, last_name1 varchar(30) default '0';
 
-	declare done int default false;
-	declare passenger_cursor cursor for select id, age, first_name, last_name from temp_passenger_booking;
-	declare continue handler for sqlstate '02000' set done = true;	
-
-	select count(*) into contact from temp_passenger_booking where id = contact_id;
-
-	#get number of bookings
-	select count(*) into booking_amount
-	from temp_passenger_booking;
-
-	#get payed seats
+	#Number of paid for seats
 	select count(*) into ticket_amount
 	from ba_ticket t
 	where flight_id = t.flight_id;
 
-	#check that contact is in booking
-	select count(p.id) into contact
-	from temp_passenger_booking p
-	where p.id = contact_id;
-
 	#reserve seats if tickets is less than 60
-	if ticket_amount <= (60 - booking_amount) and contact > 0 then
-
-		insert into ba_booking(flight_id) values (flight_id);
-		select last_insert_id() into booking_id;
-
-		open passenger_cursor;
-		repeat
-
-			#insert 
-			fetch passenger_cursor into id1, age1, first_name1, last_name1;
-			if not done then
-				insert into ba_passenger(booking_id, age, first_name, last_name) 
-					values(booking_id, age1, first_name1, last_name1);
-			end if;
-
-			if contact_id = id1 then
-				insert into ba_contact(passenger_id, phone_number, email) 
-					values(last_insert_id(), contact_phone_number, contact_email);
-			end if;
-
-		until done end repeat;
-		close passenger_cursor;
+	if ticket_amount <= (60 - booking_amount) then
+		insert into ba_booking(flight_id, amount) values (flight_id, booking_amount);
 	end if;
-end;//
+end//
 
 delimiter ;
